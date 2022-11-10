d4831a1141bf92b0daac46fd3edfef92b63c6376
alexgicimo <gakurualexgicimo@gmail.com>
2022-11-10 13:29:09 +0300

    Home landing page

---
+++ kikao_census/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>WELCOME Kikao Census SYSTEM</h1>
+      <h2>For the Future </h2>
+    
+      <button className="join us">Get Counted</button>
+    </div>
+  );
+};
+export default Home;
+
